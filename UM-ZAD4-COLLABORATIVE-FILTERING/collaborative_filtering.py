35c08e4b0ad161f1a7411d2efcac20b89747ae80
Oskar Trela <boskichlopaczyna@gmail.com>
2025-11-30 20:32:26 +0100

    Add bias parameters to collaborative filtering classifier

--- UM-ZAD4-COLLABORATIVE-FILTERING/collaborative_filtering.py
+++ UM-ZAD4-COLLABORATIVE-FILTERING/collaborative_filtering.py
@@ -18,12 +18,15 @@ class CollaborativeFiltering:
         self.num_movies = len(unique_movies)
         self.movies_dict = {movie_id: index for index, movie_id in enumerate(unique_movies)}
 
+        self.global_bias = np.mean([rating for ratings in users_ratings.values() for rating in ratings.values()])
+        self.user_biases = np.zeros(self.num_users)
+        self.movie_biases = np.zeros(self.num_movies)
+
     def train(self, epoch=100):
         self.U = np.random.rand(self.num_users, self.num_factors)
         self.M = np.random.rand(self.num_movies, self.num_factors)
 
         for epoch_num in range(epoch):
-            logging.info(f"Epoch {epoch_num} training started...")
             for user_id, ratings_dict in self.users_ratings.items():
                 for movie_id, rating in ratings_dict.items():
                     u = self.users_dict[user_id]
@@ -37,12 +40,26 @@ class CollaborativeFiltering:
 
                     self.U[u] += self.learning_rate * (error * m_vec - self.regularization_strength * u_vec)
                     self.M[m] += self.learning_rate * (error * u_vec - self.regularization_strength * m_vec)
+                    self.user_biases[u] += self.learning_rate * (error - self.regularization_strength * self.user_biases[u])
+                    self.movie_biases[m] += self.learning_rate * (error - self.regularization_strength * self.movie_biases[m])
+
+            logging.info(f"Epoch {epoch_num} loss: {self.compute_mse()}")
 
     def predict_raw(self, user_id, movie_id):
         u = self.users_dict[user_id]
         m = self.movies_dict[movie_id]
-        return np.dot(self.U[u], self.M[m])
+        return (self.global_bias
+                + self.user_biases[u]
+                + self.movie_biases[m]
+                + np.dot(self.U[u], self.M[m]))
 
     def predict(self, user_id, movie_id):
         pred = self.predict_raw(user_id, movie_id)
         return max(0, min(5, int(round(pred))))
+
+    def compute_mse(self):
+        error = 0
+        for user_id, ratings_dict in self.users_ratings.items():
+            for movie_id, rating in ratings_dict.items():
+                error += (rating - self.predict_raw(user_id, movie_id)) ** 2
+        return np.sqrt(error)
