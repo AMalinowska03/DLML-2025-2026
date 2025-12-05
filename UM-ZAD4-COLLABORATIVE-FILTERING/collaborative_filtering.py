f51171d7846f5b9d9f06605ea6a7337fb8e921eb
Oskar Trela <boskichlopaczyna@gmail.com>
2025-12-05 11:48:51 +0100

    Remove biases and normalize ratings in collaborative filtering

--- UM-ZAD4-COLLABORATIVE-FILTERING/collaborative_filtering.py
+++ UM-ZAD4-COLLABORATIVE-FILTERING/collaborative_filtering.py
@@ -18,14 +18,16 @@ class CollaborativeFiltering:
         self.num_movies = len(unique_movies)
         self.movies_dict = {movie_id: index for index, movie_id in enumerate(unique_movies)}
 
-        self.global_bias = np.mean([rating for ratings in users_ratings.values() for rating in ratings.values()])
-        self.user_biases = np.zeros(self.num_users)
-        self.movie_biases = np.zeros(self.num_movies)
+        self.user_means = {self.users_dict[u]: np.mean(list(ratings.values())) for u, ratings in users_ratings.items()}
+        self.normalized_ratings = {
+            u: {m: (r - self.user_means[self.users_dict[u]]) for m, r in ratings.items()}
+            for u, ratings in users_ratings.items()
+        }
 
         self.flattened_ratings = [
-            (self.users_dict[u], self.movies_dict[m], rating)
+            (self.users_dict[u], self.movies_dict[m], self.normalized_ratings[u][m])
             for u, ratings in self.users_ratings.items()
-            for m, rating in ratings.items()
+            for m, _ in ratings.items()
         ]
 
     def train(self, epoch=100):
@@ -43,21 +45,17 @@ class CollaborativeFiltering:
 
                 self.U[user_idx] += self.learning_rate * (error * m_vec - self.regularization_strength * u_vec)
                 self.M[movie_idx] += self.learning_rate * (error * u_vec - self.regularization_strength * m_vec)
-                self.user_biases[user_idx] += self.learning_rate * (error - self.regularization_strength * self.user_biases[user_idx])
-                self.movie_biases[movie_idx] += self.learning_rate * (error - self.regularization_strength * self.movie_biases[movie_idx])
 
             logging.info(f"Epoch {epoch_num} loss: {self.compute_rmse()}")
 
     def predict_raw(self, user_idx, movie_idx):
-        return (self.global_bias
-                + self.user_biases[user_idx]
-                + self.movie_biases[movie_idx]
-                + np.dot(self.U[user_idx], self.M[movie_idx]))
+        return np.dot(self.U[user_idx], self.M[movie_idx])
 
     def predict(self, user_id, movie_id):
         user_idx = self.users_dict[user_id]
         movie_idx = self.movies_dict[movie_id]
         pred = self.predict_raw(user_idx, movie_idx)
+        pred += self.user_means[user_idx]
         return max(0, min(5, int(round(pred))))
 
     def compute_rmse(self):
